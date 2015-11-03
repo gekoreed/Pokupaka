3d8dbbf3b6cf02b0be128b25a2df7aef6c7497c1
Evgen Shevchenko <gekoreed@gmail.com>
2015-11-03 19:34:19 +0200

    remover fixed

--- DataServer/src/main/java/com/selfach/service/OldImagesRemover.java
+++ DataServer/src/main/java/com/selfach/service/OldImagesRemover.java
@@ -7,9 +7,13 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
+import java.sql.Timestamp;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 /**
  * By gekoreed on 9/26/15.
@@ -24,24 +28,31 @@ public class OldImagesRemover {
 
     @Scheduled(fixedDelay = 60 * 60 * 1000)
     public void removaOldImages() throws Exception {
-        Date d = new Date();
+        Timestamp dayBefore = Timestamp.valueOf(LocalDateTime.now().minusDays(1));
 
-        Arrays.asList(new File("pictures").list()).stream()
-                .filter(s -> !s.equals("c"))
-                .filter(file -> {
-                    String[] splitted = file.split("-");
-                    String nanoTime = splitted[splitted.length - 2];
-                    Long str = Long.valueOf(nanoTime.substring(0, 10));
-                    Long date = Long.valueOf(new SimpleDateFormat("yyyyMMddHH").format(d));
-                    return date - str > 100;
-                })
-                .map(photoName -> {
-                    boolean delete = new File("pictures/" + photoName).delete();
-                    boolean delete2 = new File("pictures/c/" + photoName).delete();
-                    photoDao.deletePhoto(photoName.split("-")[1]);
-                    return !delete || !delete2;
-                }).findAny().ifPresent((c) -> logger.info("not all images were removed"));
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
 
+        String[] names = new File("pictures").list();
+
+        for (int i = 0; i < names.length; i++) {
+            String name = names[i];
+            if (name.equals("c"))
+                continue;
+            String[] splitted = name.split("-");
+            String nanoTime = splitted[splitted.length - 2];
+            try {
+                Timestamp made = new Timestamp(format.parse(nanoTime).getTime());
+                if (dayBefore.after(made)) {
+                    logger.info("Deleting:        " + name);
+                    boolean delete = new File("pictures/" + name).delete();
+                    boolean delete2 = new File("pictures/c/" + name).delete();
+                    if (!delete || !delete2)
+                        logger.info("Shit happens");
+                    photoDao.deletePhoto(nanoTime);
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
 }
